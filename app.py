9fa8aa38e25ea0cbb253711875ab3b2b49e092af
vihaanpatel2507-sudo <vihaanpatel2507@gmail.com>
2026-04-07 13:01:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="vihaan@2507",  # change if needed
+        password="vihaan@2507",  
         database="college_event"
     )
 
